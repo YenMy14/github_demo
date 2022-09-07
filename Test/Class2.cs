b9c4958041d225e4c2102818eb2c9262ccf406e4
Ly Mai <maithily14122001@gmail.com>
2022-09-07 10:29:16 +0700

    Demo git

---
+++ Test/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mai Thi Ly !");
+             Console.WriteLine("Mai Thi Ly da them dong nay!");
+        }
+    }
+}
---
+++ Test/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Class3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello moi nguoi!");
+        }
+    }
+}
